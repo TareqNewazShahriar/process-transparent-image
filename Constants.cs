0461dae50c8466c162dbfdc7be7676956ec480a8
Tareq Newaz Shahriar <tareq.newaz.shahriar@gmail.com>
2024-02-09 21:53:31 +0600

    First commit. Opaque image with smooth edge.

---
+++ Constants.cs
@@ -0,0 +1,16 @@
+namespace Company.Function;
+
+public class Constants
+{
+   public static string ImageUrlKeyLower => "image-url";
+   public static string FillTransparencyKeyLower => "fill-transparency";
+   public static string SmoothPixelKeyLower => "smooth-edging";
+
+
+   public enum FillTransparency
+   {
+      Black,
+      White,
+      None
+   }
+}
---
+++ ImageProcessing.cs
@@ -0,0 +1,138 @@
+using Company.Function;
+using SkiaSharp;
+using System.Drawing;
+
+public class ImageProcessing : IDisposable
+{
+    private SKBitmap _image = null; // origin image
+
+    public ImageProcessing(byte[] imageBytes)
+    {
+        _image = FromByteToSkiaImage(imageBytes);
+    }
+
+    // convert byte array to skbitmap using skiashap librar
+    public SKBitmap FromByteToSkiaImage(byte[] imageBytes)
+    {
+        // create a memory stream from the byte array
+        var stream = new MemoryStream(imageBytes);
+        stream.Position = 0;
+        // create a bitmap from the memory stream
+        var bitmap = SKBitmap.Decode(stream);
+
+        return bitmap;
+    }
+
+    // check transparency of an image using Skiashap library
+    public void SetColorToTransparentPixels(Constants.FillTransparency fillTransparencyValue, bool smoothEdging)
+    {
+        int countTransparentPixels = 0;
+        SKColor requestedColor = new SKColor();
+
+        if (fillTransparencyValue == Constants.FillTransparency.White)
+            requestedColor = new SKColor(byte.MaxValue, byte.MaxValue, byte.MaxValue, byte.MaxValue);
+        else if (fillTransparencyValue == Constants.FillTransparency.Black)
+            requestedColor = new SKColor(0, 0, 0, byte.MaxValue);
+
+        // loop through each pixel in the image and make it fully opaque if it is not
+        for (int y = 0; y < _image.Height; y++)
+        {
+            for (int x = 0; x < _image.Width; x++)
+            {
+                // get the color of the pixel
+                var color = _image.GetPixel(x, y);
+
+                // if the pixel has transparency
+                if (color.Alpha < byte.MaxValue)
+                {
+                    if (color.Alpha == byte.MinValue)
+                    {
+                        _image.SetPixel(x, y, requestedColor);
+                    }
+                    else
+                    {
+                        if (smoothEdging)
+                            _image.SetPixel(x, y, SmoothPixel(color, fillTransparencyValue));
+                        else
+                            _image.SetPixel(x, y, new SKColor(color.Red, color.Green, color.Blue, 255)); // just make the pixel opaque
+                    }
+
+                    //countTransparentPixels++;
+                }
+            }
+        }
+    }
+
+    public byte[] SkBitmapToByteArray()
+    {
+        var imageStream = new MemoryStream();
+
+        // convert the image to a byte array
+        _image.Encode(imageStream, SKEncodedImageFormat.Png, 100);
+
+        imageStream.Position = 0;
+        // memorystream to byte array
+        byte[] imageBytes = imageStream.ToArray();
+
+        return imageBytes;
+    }
+
+    // if an image contains transparent pixels using skiashap library
+    public SKBitmap FromByteToSkiaImageUsingSkCodec(byte[] imageBytes)
+    {
+        // get height and width of the image from byte array using skiashap library
+        var imageInfo = SKImageInfo.Empty;
+        
+        using (var stream = new MemoryStream(imageBytes))
+        using (var codec = SKCodec.Create(stream))
+        {
+            imageInfo = codec.Info;
+        }
+
+        // create an empty bitmap
+        var bitmap = new SKBitmap(imageInfo);
+
+        return bitmap;
+    }
+
+    public byte[] ReturnImageBytes()
+    {
+        var bytes = SkBitmapToByteArray();
+        Dispose();
+
+        return bytes;
+    }
+
+    public void Dispose()
+    {
+        _image.Dispose();
+    }
+
+    // Make the color lighter or darker according to the fill-transparency value
+    private SKColor SmoothPixel(SKColor color, Constants.FillTransparency fillTransparency)
+    {
+        SKColor newColor = color;
+        int howLessIsAlpha = (byte.MaxValue - color.Alpha) / 2;
+
+        if (fillTransparency == Constants.FillTransparency.White)
+        {
+            // make the color lighter
+            newColor = new SKColor(
+                (byte)(color.Red + howLessIsAlpha < byte.MaxValue ? color.Red + howLessIsAlpha : byte.MaxValue),
+                (byte)(color.Green + howLessIsAlpha < byte.MaxValue ? color.Green + howLessIsAlpha : byte.MaxValue),
+                (byte)(color.Blue + howLessIsAlpha < byte.MaxValue ? color.Blue + howLessIsAlpha : byte.MaxValue),
+                byte.MaxValue);
+        }
+        else if (fillTransparency == Constants.FillTransparency.Black)
+        {
+            // make the color darker
+            newColor = new SKColor(
+                (byte)(color.Red - howLessIsAlpha > byte.MinValue ? color.Red - howLessIsAlpha : byte.MaxValue),
+                (byte)(color.Green - howLessIsAlpha > byte.MinValue ? color.Green - howLessIsAlpha : byte.MaxValue),
+                (byte)(color.Blue - howLessIsAlpha > byte.MinValue ? color.Blue - howLessIsAlpha : byte.MaxValue),
+                byte.MaxValue);
+        }
+
+        return newColor;
+    }
+}
