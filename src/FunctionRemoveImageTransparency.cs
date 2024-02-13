2a64fd9ce0965b3ea94cc7f47920e2791b81579d
Tareq Newaz Shahriar <tareq.newaz.shahriar@gmail.com>
2024-02-13 20:14:08 +0600

    Add files via upload

--- src/FunctionRemoveImageTransparency.cs
+++ src/FunctionRemoveImageTransparency.cs
@@ -95,7 +95,7 @@ public class FunctionRemoveTransparency
     {
         Uri uri = new Uri(uriString);
         using var httpClient = new HttpClient();
-        httpClient.DefaultRequestHeaders.Add("User-Agent", "Azure Function v4; .Net8-C#;");
+        httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:122.0) Gecko/20100101 Firefox/122.0");
 
         // Download the image and convert to byte array
         var response = await httpClient.GetAsync(uri);
